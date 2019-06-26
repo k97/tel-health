cb714b2740c1f5adcfb59ff570d6c40a1f41f728
Karthik <karthik97live@gmail.com>
2019-06-27 04:22:57 +1000

    Adding build files as docs for gh-pages

---
+++ docs/precache-manifest.be04692fa78624eaece729b9a952749c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dddfef4f2b2a7d851a29d66f4acd2368",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b024947b85ed53b9a2dc",
+    "url": "/static/css/2.1719fe29.chunk.css"
+  },
+  {
+    "revision": "383b1584d5ea31b985e2",
+    "url": "/static/css/main.dcd282aa.chunk.css"
+  },
+  {
+    "revision": "b024947b85ed53b9a2dc",
+    "url": "/static/js/2.58f23762.chunk.js"
+  },
+  {
+    "revision": "383b1584d5ea31b985e2",
+    "url": "/static/js/main.2acf4c63.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
